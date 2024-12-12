54c7bc791d2ac9e5a6b7386dc21e9baaea01be3d
Hien Nguyen Dang <161845031+fireflycute945@users.noreply.github.com>
2024-12-12 21:18:32 +0700

    UDP Version 1.3
    CHắc không ai thèm đọc cập nhật mỗi phiên bản đâu.
    Nên từ phiên bản 1.4 trở đi sẽ không còn file ghi lại nội dung đã cập nhật.

--- UDP/server.py
+++ UDP/server.py
@@ -1,4 +1,6 @@
 import socket
+import time # Thêm thư viện time, dùng để tạo delay
+import os # Thêm thư viện os, dùng để thao tác với hệ thống file
 import serverConfig # Thêm file serverConfig.py vào thư mục chứa file server.py
 
 '''
@@ -22,17 +24,40 @@ def main():
     port = serverConfig.port
     file_path = serverConfig.file_path    
     
+    processing_line = [] # Khởi tạo list để lưu các dòng đã xử lí
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Tạo socket với IPv4 và UDP
     s.bind((host, port)) # Bind(lắng nghe) địa chỉ và port
     print("UDP server is running...") #Báo hiệu server đang chạy
 
     while True:
-        data, addr = s.recvfrom(1024) # Nhận dữ liệu từ client với buffer size = 1024
+        data, addr = s.recvfrom(1024 * 16) # Nhận dữ liệu từ client với buffer size = 1024 * 16
         print(f"Received request from {addr}") # Gửi thông báo đã nhận yêu cầu từ client
         
-        if data.decode(): # Kiểm tra dữ liệu nhận được
-            send_file(s, addr, file_path + data.decode()) # Gửi file
-            print(f"Sent file to {addr}") # Gửi thông báo đã gửi file thành công
+        if data.decode(): # Kiểm tra dữ liệu nhận được có giá trị không
+            if not os.path.exists(file_path + data.decode()): # Kiểm tra file tồn tại
+                s.sendto(b'File not found', addr) # Gửi thông báo file không tồn tại
+                continue
+            if (file_path + data.decode()) not in processing_line: # Kiểm tra file đã được xử lí chưa
+                processing_line.append(file_path + data.decode()) # Thêm file đã xử lí vào list
+            with open(file_path + data.decode(), 'rb') as f: # Mở file ở chế độ đọc nhị phân, with open sẽ tự đóng file sau khi kết thúc block
+                while True: 
+                    chunk = f.read(1024 * 16) # buffer size = 1024 * 16
+                    if not chunk:  # Xử lí trường hợp không còn dữ liệu
+                        break 
+                    while True:
+                        s.sendto(chunk, addr) # Gửi dữ liệu
+                        data, addr = s.recvfrom(1024 * 16) # Nhận dữ liệu từ client với buffer size = 1024 * 16
+                        if data == b'OK':
+                            break
+        print(f"Sent file to {addr}") # Gửi thông báo đã gửi file thành công
+        s.sendto(b'EOF', addr) # Gửi tín hiệu kết thúc file
+        time.sleep(5) # Đợi 5 giây trước khi kiểm tra lại file input.txt
+        
+        # Trường hợp processing_line quá lớn, có thể xóa đi để giảm bộ nhớ
+        if (len(processing_line) > 10000):
+            processing_line.clear()
+        
 
 if __name__ == "__main__": # Chạy hàm main
     main()
